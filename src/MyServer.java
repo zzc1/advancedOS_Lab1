00a88c5b4b3951414881612df770e90eca0644c3
NJFU.ZZC <a1506135094@gmail.com>
2021-04-08 19:07:06 +0800

    update

--- src/MyServer.java
+++ src/MyServer.java
@@ -1,35 +1,41 @@
 import java.io.IOException;
 import java.net.*;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 
 public class MyServer extends Thread {
+//所有机器都认定的时间t0
     private static int t0 = 0;
+//再同步间隔参数R
     private static int R = 100;
+//S?
     private static int S = 1;
+//初始化本地广播地址 255.255.255.255
     private static InetAddress inetAddr;
-
     static {
         try {
-            // 在Java UDP中单播与广播的代码是相同的,要实现具有广播功能的程序只需要使用广播地址即可, 例如：这里使用了本地的广播地址
             inetAddr = InetAddress.getByName("255.255.255.255");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
-
+//总的机器数量
     private static int serverNums = 0;
-    private Double current;
-    private int serverId;
-    private int serverPort;
-    private Double step;
 
-    private DatagramPacket receive;
-    private DatagramSocket server;
+//用一个浮点数表示机器当前时间
+    private Double current;
+//机器id号
+    private final int serverId;
+//时间增加的步长
+    private final Double step;
+
+    private final DatagramPacket receive;
+    private final DatagramSocket server;
     private DatagramSocket client;
 
     public MyServer(int serverId, int serverPort, Double step) throws SocketException {
         this.serverId = serverId;
-        this.serverPort = serverPort;
         this.current = .0 + serverId;
         this.step = step;
         serverNums++;
@@ -37,16 +43,16 @@ public class MyServer extends Thread {
         server = new DatagramSocket(serverPort);
         client = new DatagramSocket();
     }
-
     @Override
     public void run() {
         while (true) {
             current += step;
             try {
-                Thread.sleep(10);
+                TimeUnit.MILLISECONDS.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+//            广播当前时间
             if ((Math.ceil(current) - t0) % R == 0) {
                 byte[] msg = current.toString().getBytes();
                 System.out.println(serverId + "th Server before synchronize time:"+current);
@@ -66,16 +72,15 @@ public class MyServer extends Thread {
                     }
                 }
                 client.close();
+//               监听其他机器广播的时间
                 int k = 0;
-                Double sum = .0;
+                double sum = .0;
                 while (true) {
                     try {
                         server.receive(receive);
                         byte[] recvByte = Arrays.copyOfRange(receive.getData(), 0,
                                 receive.getLength());
-//                        System.out.println(serverId + "th Server receive msg:" + Double.valueOf(new String(recvByte)));
-                        sum += Double.valueOf(new String(recvByte));
-//                        System.out.println("sum:"+sum);
+                        sum += Double.parseDouble(new String(recvByte));
                         k++;
                         if (k == serverNums) {
                             current = sum / serverNums;
